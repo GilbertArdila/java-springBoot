31f49a4d4b95a9f6d06db2df8bcc6ab71ed2e5e5
Gilbert Ardila <gilbertferney@gmail.com>
2023-03-12 14:31:21 -0500

    query methods

--- src/main/java/com/platzi/fundamentos/FundamentosApplication.java
+++ src/main/java/com/platzi/fundamentos/FundamentosApplication.java
@@ -35,15 +35,15 @@ public class FundamentosApplication implements CommandLineRunner {
 	private void saveUsersInDB(){
 		try{
 			log.info("Inicio de datos dummi");
-			User user1 = new User("John", "john@domain.com", LocalDate.of(2021, 3, 13));
-			User user2 = new User("Marco", "marco@domain.com", LocalDate.of(2021, 12, 8));
-			User user3 = new User("Daniela", "daniela@domain.com", LocalDate.of(2021, 9, 8));
-			User user4 = new User("Marisol", "marisol@domain.com", LocalDate.of(2021, 6, 18));
+			User user1 = new User("John", "john@domain.com", LocalDate.of(2016, 3, 13));
+			User user2 = new User("Marco", "marco@domain.com", LocalDate.of(2017, 12, 8));
+			User user3 = new User("Daniela", "daniela@domain.com", LocalDate.of(2019, 9, 8));
+			User user4 = new User("Marisol", "marisol@domain.com", LocalDate.of(2020, 6, 18));
 			User user5 = new User("Karen", "karen@domain.com", LocalDate.of(2021, 1, 1));
-			User user6 = new User("Carlos", "carlos@domain.com", LocalDate.of(2021, 7, 7));
+			User user6 = new User("Carlos", "carlos@domain.com", LocalDate.of(2018, 7, 7));
 			User user7 = new User("Enrique", "enrique@domain.com", LocalDate.of(2021, 11, 12));
-			User user8 = new User("Luis", "luis@domain.com", LocalDate.of(2021, 2, 27));
-			User user9 = new User("Paola", "paola@domain.com", LocalDate.of(2021, 4, 10));
+			User user8 = new User("Luis", "luis@domain.com", LocalDate.of(2016, 2, 27));
+			User user9 = new User("Paola", "paola@domain.com", LocalDate.of(2019, 4, 10));
 			User user10 = new User("Luis", "luis2@domain.com", LocalDate.of(2021, 8, 27));
 			User user11 = new User("Luis carlos", "luiscarlos@domain.com", LocalDate.of(2021, 5, 27));
 			List<User> list = Arrays.asList(user1,user2,user3,user4,user5,user6,user7,user8,user9,user10,user11);
@@ -59,23 +59,45 @@ public class FundamentosApplication implements CommandLineRunner {
 	private void getInfoJPQLFromUser(){
 		log.info( "Esta es la información del usuario buscado"+userRepository.findByEmail("luis@domain.com")
 				.orElseThrow(() ->new RuntimeException("No hay usuarios con ese mail")));
+
+
 	}
 
 	private void getAndSort(){
 		try{
-			userRepository.findAndSort("Luis", Sort.by("birthday").descending()).stream()
+			userRepository.findAndSort("Luis", Sort.by("birthday").descending())
 					.forEach(user -> log.info("Información de la lista "+user));
+
 		}catch (Exception e){
 			log.debug("Error en el método buscar y ordenar "+e);
 
 		}
 	}
+	private void getByName(){
+		try{
+			userRepository.findByName("Luis")
+					.forEach(user -> log.info(("Usuario por nombre "+user)));
 
+	}catch (Exception e){
+		log.error(e);
+		}
+	}
+	//para usar Like se debe pasar la sentencia en medio de %nombre%
+   private void getByNameLike(){
+	   userRepository.findByNameLike("%Ma%").forEach(user -> log.info("Nombre like "+user));
+	   userRepository.findByNameOrEmail(null,"luis2@domain.com")
+			   .forEach(user -> log.info("User by mail or name"+user));
+	   userRepository.findByBirthdayBetween(LocalDate.of(2016,05,15),
+					   LocalDate.of(2019,03,18))
+			   .forEach(user -> log.info("Usuarios por fecha "+user));
+   }
 
 	@Override
 	public void run(String... args) throws Exception {
 		saveUsersInDB();
 		getInfoJPQLFromUser();
 		getAndSort();
+		getByName();
+		getByNameLike();
 	}
 }
--- src/main/java/com/platzi/fundamentos/repository/UserRepository.java
+++ src/main/java/com/platzi/fundamentos/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +17,15 @@ public interface UserRepository extends JpaRepository<User ,Long> {
     Optional<User> findByEmail(String email);
     @Query("SELECT u FROM User u WHERE u.name LIKE ?1%")
     List<User> findAndSort(String name, Sort sort);
+//query methods
+    List<User> findByName(String name);
+
+    List<User> findByNameLike(String name);
+   List<User> findByNameOrEmail(String name,String email);
+
+   List<User> findByBirthdayBetween(LocalDate begin,LocalDate end);
+
+   List<User>findByNameLikeOrderByDesc(String name);
 
 
 
